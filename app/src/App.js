1586c68535cd992ada3a85fd465850ff49a176e4
shuufujita <fuchantoshu@gmail.com>
2022-12-24 23:21:01 +0900

    add: 体重登録機能を追加

---
+++ app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import AppContainer from './pages/AppContainer';
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: "Weight Manager"
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header title={this.state.title}></Header>
+        <AppContainer></AppContainer>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ app/src/components/InputNumber.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Style from "./InputNumber.module.css";
+
+class InputNumber extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            label: props.label ? props.label : "kk",
+            value: 0
+        }
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(event) {
+        this.setState({value: event.target.value});
+        this.props.change(event.target.value);
+    }
+
+    render() {
+        const {label} = this.state;
+        return (
+            <div className={Style.inputItem}>
+                <input className={Style.weightNumber} type="number" value={this.state.value} onChange={this.onChange}></input> {label}
+            </div>
+        );
+    }
+}
+
+export default InputNumber;
